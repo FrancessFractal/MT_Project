f2262f67567a9312598a6d010fe203e5ef1365f7
Caroline <caroline.appleby@cantab.net>
2016-05-23 11:46:40 +0200

    what the methods should do in comments

--- SentenceReorderer.java
+++ SentenceReorderer.java
@@ -85,6 +85,12 @@ public class SentenceReorderer {
             List<Tree> children = parse.getChildrenAsList();
             // TODO: manipulate children
 
+            // Tree head = first(leftmost) child in children where child.label startswith V  (java doesnt have a nice way to do this?!)
+            // assuming for now there will be only 1 V tag anyways
+            // or if there are many we care about the leftmost only
+
+            // insert head into first position in children array  (is there a way to do this without making a new List and slicing and shit?)
+
             parse.setChildren(children);
         }
 
@@ -98,23 +104,145 @@ public class SentenceReorderer {
     }
 
     public static void verb_second(Tree parse) {
+        // do main logic of the rule here
+        // rule will happen for every node in tree meeting criteria below
 
+        // List<String> conjunction_tags = ["KOUS", "PWAV", "PWS", "PRELS", "PRELAT"]
+        //if (parse.label().value().equals("S")) {
+            // List<Tree> children = parse.getChildrenAsList();
+            // if child in conjunction_tags {
+
+                // do the thing!
+                // Tree head = first(leftmost) child in children where child.label startswith V
+                // assuming for now there will be only 1 V tag anyways
+                // or if there are many we care about the leftmost only
+
+                // insert head into second position in children ( or immediately after conj, whichever is easier to do)
+
+            parse.setChildren(children);
+        }
+
+        // recursively call this on the whole tree to perform rule on all Ss
+        if ( ! parse.isLeaf() ) {
+            Tree[] children = parse.children();
+            for (Tree child : children) {
+                SentenceReorderer.verb_second(child);
+            }
+        }
     }
 
+}
+
     public static void subject(Tree parse) {
+        // do main logic of the rule here
+        // rule will happen for every node in tree meeting criteria below
+        if (parse.label().value().equals("S")) {
+
+            List<Tree> children = parse.getChildrenAsList();
+            // TODO: manipulate children
+
+            // Tree head = first(leftmost) child in children where child.label startswith V  (this gets done 3 times, should be separate subroutine?)
+            // assuming for now there will be only 1 V tag anyways
+            // or if there are many we care about the leftmost only
+
+            // Tree subject = leftmost child in children where child.label = [NP or PPER]
+
+            // insert subject into children at position immediately before head
+            parse.setChildren(children);
+        }
+
+        // recursively call this on the whole tree to perform rule on all Ss
+        if ( ! parse.isLeaf() ) {
+            Tree[] children = parse.children();
+            for (Tree child : children) {
+                SentenceReorderer.subject(child);
+            }
+        }
 
     }
 
     public static void particles(Tree parse) {
+        // do main logic of the rule here
+        // rule will happen for every node in tree meeting criteria below
 
-    }
+        //if (parse.label().value().equals("S")) {
+            // List<Tree> children = parse.getChildrenAsList();
+            // if (child.label == "VVFIN" or "VAFIN" or "VMFIN") AND (child.label == "PTKVZ") in children  {
+
+                // do the thing!
+                // move the PTKVZ child to immediately before V*FIN
+
+            parse.setChildren(children);
+            }
+
+        // recursively call this on the whole tree to perform rule on all Ss
+        if ( ! parse.isLeaf() ) {
+            Tree[] children = parse.children();
+            for (Tree child : children) {
+                SentenceReorderer.particles(child);
+                }
+            }
+        }
 
     public static void infinitives(Tree parse) {
+        // do main logic of the rule here
+        // rule will happen for every node in tree meeting criteria below
+        parse = tree_flatten(parse)
+        if (parse.label().value().equals("S")) {
+
+            List<Tree> children = parse.getChildrenAsList();
+            // TODO: manipulate children
+
+                // if children contains (child.label == "VVFIN" or "VAFIN" or "VMFIN") = Tree vfin
+                // AND (child.label == "VVINF" or VVIMP VVIZU VVPP VAIMP VAINF VAPP VMINF VMPP) = Tree vnonfin
+
+                    // move vnonfin to immediately after vfin
+
+            parse.setChildren(children);
+        }
+
+        // recursively call this on the whole tree to perform rule on all Ss
+        if ( ! parse.isLeaf() ) {
+            Tree[] children = parse.children();
+            for (Tree child : children) {
+                SentenceReorderer.infinitives(child);
+                }
+            }
+        }
 
     }
 
     public static void negation(Tree parse) {
+        // do main logic of the rule here
+        // rule will happen for every node in tree meeting criteria below
+        if (parse.label().value().equals("S")) {
 
-    }
+            List<Tree> children = parse.getChildrenAsList();
+            // TODO: manipulate children
+
+            // if children contains (child.label == "VVFIN" or "VAFIN" or "VMFIN") = Tree vfin
+            // AND (child.label == "VVINF" or VVIMP VVIZU VVPP VAIMP VAINF VAPP VMINF VMPP) = Tree vnonfin
+            // AND (child.label == "PTKENEG") = negpart
+
+                // move negpart to immediately after vfin
+
+            parse.setChildren(children);
+        }
+
+        if ( ! parse.isLeaf() ) {
+            Tree[] children = parse.children();
+            for (Tree child : children) {
+                SentenceReorderer.negation(child);
+            }
+        }
 
 }
+
+
+    public static Tree tree_flatten(Tree parse) {
+        if (parse.label().value().equals("VP")) {
+            List<Tree> children = parse.getChildrenAsList();
+            // for child in children:
+                // child.parent = parse.parent
+            // remove the parse subtree from the sentence (delete this node or something)
+        }
